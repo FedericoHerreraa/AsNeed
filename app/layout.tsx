3c2ec16c1987a91d3cb39b9f5558326b741c04d2
Federico Herrera <fede.juan.herrera@gmail.com>
2025-04-25 17:11:55 -0300

    adding google-site-verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="nVV8ikXo8XJRDMFT3lBj37jJhx0dKodDvmukZSFMKzs" />
         <link rel="icon" href="https://tyvgqomjokwdofxjtxbc.supabase.co/storage/v1/object/sign/logos/LogoWeb.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsb2dvcy9Mb2dvV2ViLnBuZyIsImlhdCI6MTc0MzY4Mjk1MSwiZXhwIjoxOTAxMzYyOTUxfQ.HHn-uuKvP_5MCGsbowpwQ8_blSEWR9TBsOophML5vrQ"/>
       </head>
       <body>
