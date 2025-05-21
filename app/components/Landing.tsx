189961e30ed45c65f289190f8051a5046a492b13
Federico Herrera <fede.juan.herrera@gmail.com>
2025-05-21 18:08:50 -0300

    changing landing

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -44,30 +44,32 @@ export const LandingInfo = () => {
                 fill
             />
 
-            <div className='absolute inset-0 bg-gradient-to-r dark:from-black/100 dark:via-black/80 dark:to-transparent dark:z-10 from-white/100 via-white/80 to-transparent z-10' />
+            <div className='absolute inset-0 bg-gradient-to-b dark:from-black/100 dark:via-black/80 dark:to-transparent dark:z-10 from-white/100 via-white/80 to-transparent z-10' />
             <div className='absolute bottom-0 left-0 w-full h-32 bg-gradient-to-b from-transparent dark:to-black z-10 to-white' />
+            
             <div className="absolute top-0 left-0 w-full z-30">
                 <Header bgDark="transparent" bgLight="transparent" />
             </div>
-            <div className="relative z-20 w-[80%] mx-auto flex justify-between gap-10 h-full items-center">
-                <div className="flex flex-col md:gap-10 gap-7 md:w-1/2 md:items-start items-center">
-                    <h1 className='md:text-6xl text-4xl md:text-left text-center font-semibold dark:text-white text-black'>
-                        {spanish ? 'Comenzá tu' : 'Start Your Own'}{' '}
-                        <span className="bg-gradient-to-r from-blue-600 to-violet-600 bg-clip-text text-transparent">
+            <div className="relative z-20 mx-auto flex justify-center gap-10 h-full items-center">
+                <div className="flex flex-col md:gap-10 gap-7 md:w-1/2 items-center mt-20">
+                    <h1 className='md:text-7xl text-4xl text-center font-semibold dark:text-white text-black'>
+                        {spanish ? 'Mejorá tu' : 'Improve Your'}{' '}
+                        <span className="bg-gradient-to-r dark:from-blue-600 dark:to-violet-600 from-blue-500 to-violet-500 bg-clip-text text-transparent">
                             {spanish ? 'Negocio' : 'Digital'}{' '}
                         </span>
-                        <span ref={typedRef}></span>
+                        <br /> 
+                        <span ref={typedRef} className="text-center bg-gradient-to-r dark:from-violet-500 dark:to-violet-800 from-blue-500 to-violet-600 bg-clip-text text-transparent"></span>
                     </h1>
-                    <p className="md:text-xl text-md md:text-left text-center dark:text-zinc-300 text-zinc-700">
+                    <p className="md:text-xl text-md text-center dark:text-zinc-300 text-zinc-700">
                         {spanish 
-                            ? 'En "AsNeed", transformamos tus ideas en realidad digital. Diseñamos y desarrollamos aplicaciones móviles, plataformas web con IA y redes sociales, completamente personalizadas para cubrir las necesidades de tu negocio.'
-                            : 'At "AsNeed", we turn your ideas into digital reality. We design and develop mobile applications, web platforms with AI, and social media, fully customized to meet your business needs.'
+                            ? 'En "AsNeed", transformamos tus ideas en realidad digital. Diseñamos y desarrollamos sistemas de gestión, aplicaciones móviles y plataformas web con IA, completamente personalizadas para cubrir las necesidades de tu negocio.'
+                            : 'At "AsNeed", we turn your ideas into digital reality. We design and develop management systems, mobile applications and web platforms with AI, fully customized to meet your business needs.'
                         }
                     </p>
                     <div className="flex items-center gap-3">
                         <FaHandshake size={25} className="dark:text-white text-zinc-700"/>
                         <p className="md:text-lg text-md md:text-left text-center dark:text-zinc-200 text-zinc-700">
-                            {spanish ? '' : 'Trusted by '}<span className="font-bold">10+</span> {spanish ? 'compañías confían en nosotros' : 'companies'}
+                            {spanish ? '' : 'Trusted by '}<span className="font-bold">20+</span> {spanish ? 'compañías confían en nosotros' : 'companies'}
                         </p>
                     </div>
                     <Link 
